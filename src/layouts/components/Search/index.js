1c80fea3184662fdd8930257b3c33c5250796c2d
KhoaPC <luongvankhoapc@gmail.com>
2022-11-09 07:54:12 +0700

    22Nov11 UPDATE component `Search`

---
+++ src/layouts/components/Search/index.js
@@ -0,0 +1,102 @@
+import { useEffect, useState, useRef, useMemo } from 'react';
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import HeadlessTippy from '@tippyjs/react/headless';
+import clsx from 'clsx';
+
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
+import { SearchIcon } from '~/components/Icons';
+import styles from './Search.module.css';
+import { useDebounce } from '~/hooks';
+import * as searchServices from '~/services/searchService';
+
+function Search() {
+    const [searchValue, setSearchValue] = useState(''); // onChange
+    const [searchResult, setSearchResult] = useState([]);
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const debouncedValue = useDebounce(searchValue, 500); // Delay 500ms
+    const inputRef = useRef();
+
+    useEffect(() => {
+        if (!debouncedValue.trim()) {
+            setSearchResult([]);
+            return;
+        }
+
+        const fetchApi = async () => {
+            setLoading(true);
+
+            const result = await searchServices.search(debouncedValue);
+
+            setSearchResult(result);
+            setLoading(false);
+        };
+
+        fetchApi();
+    }, [debouncedValue]);
+
+    const handleClear = () => {
+        setSearchValue('');
+        setSearchResult([]);
+        inputRef.current.focus();
+    };
+
+    const handleHideResult = () => {
+        setShowResult(false);
+    };
+
+    // Ngăn việc nhập dấu cách ở đầu
+    const handleChange = (e) => {
+        const searchValue = e.target.value;
+        if (!searchValue.startsWith(' ')) {
+            setSearchValue(searchValue);
+        }
+    };
+
+    return (
+        // Using a wrapper <div> tag around the reference element solves
+        // this by creating a new parentNode context.
+        <div>
+            <HeadlessTippy
+                interactive
+                visible={showResult && searchResult.length > 0}
+                render={(attrs) => (
+                    <div className={clsx(styles.searchResult)} tabIndex="-1" {...attrs}>
+                        <PopperWrapper>
+                            <h4 className={clsx(styles.searchTitle)}>Accounts</h4>
+                            {searchResult.map((result) => (
+                                <AccountItem key={result.id} data={result} />
+                            ))}
+                        </PopperWrapper>
+                    </div>
+                )}
+                onClickOutside={handleHideResult}
+            >
+                <div className={clsx(styles.search)}>
+                    <input
+                        ref={inputRef}
+                        value={searchValue}
+                        placeholder="Search accounts and videos"
+                        spellCheck={false}
+                        onChange={handleChange}
+                        onFocus={() => setShowResult(true)}
+                    />
+                    {!!searchValue && !loading && (
+                        <button className={clsx(styles.clear)} onClick={handleClear}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                    )}
+                    {loading && <FontAwesomeIcon className={clsx(styles.loading)} icon={faSpinner} />}
+
+                    <button className={clsx(styles.searchBtn)} onMouseDown={(e) => e.preventDefault()}>
+                        <SearchIcon />
+                    </button>
+                </div>
+            </HeadlessTippy>
+        </div>
+    );
+}
+
+export default Search;
