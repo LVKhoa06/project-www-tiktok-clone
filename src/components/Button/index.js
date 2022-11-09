cb5654d757afa8514f91f0962f0a8efefa5e34fb
KhoaPC <luongvankhoapc@gmail.com>
2022-11-09 07:51:39 +0700

    22Nov11 FIX error library `clsx`

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import clsx from 'clsx';
 import styles from './Button.module.css';
@@ -25,15 +26,6 @@ function Button({
         ...passProps,
     };
 
-    // Remove event listener when btn is disabled
-    if (disabled) {
-        Object.keys(props).forEach((key) => {
-            if (key.startsWith('on') && typeof props[key] === 'function') {
-                delete props[key];
-            }
-        });
-    }
-
     if (to) {
         props.to = to;
         Comp = Link;
@@ -44,13 +36,13 @@ function Button({
 
     const classes = clsx(styles.wrapper, {
         [className]: className,
-        primary,
-        outline,
-        text,
-        disabled,
-        rounded,
-        small,
-        large,
+        [styles.primary]: primary,
+        [styles.outline]: outline,
+        [styles.text]: text,
+        [styles.disabled]: disabled,
+        [styles.rounded]: rounded,
+        [styles.small]: small,
+        [styles.large]: large,
     });
 
     return (
@@ -61,5 +53,22 @@ function Button({
         </Comp>
     );
 }
+
+Button.propTypes = {
+    to: PropTypes.string,
+    href: PropTypes.string,
+    primary: PropTypes.bool,
+    outline: PropTypes.bool,
+    text: PropTypes.bool,
+    rounded: PropTypes.bool,
+    disabled: PropTypes.bool,
+    small: PropTypes.bool,
+    large: PropTypes.bool,
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string,
+    leftIcon: PropTypes.node,
+    rightIcon: PropTypes.node,
+    onClick: PropTypes.func,
+};
 
 export default Button;
